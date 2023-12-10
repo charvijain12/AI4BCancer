87c52e17dc6c8a1dd88ca860dca9579c3cb21779
Shaurya Srinet <sn0273@srmist.edu.in>
2023-12-10 19:06:45 +0530

    Add files via upload

---
+++ Models/Models with PCA/Naive Bayes/Naive Bayes.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Import necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import classification_report, accuracy_score
+
+
+# In[2]:
+
+
+# Load the Wisconsin Breast Cancer dataset
+file_path = r"C:\Users\Shaurya\Downloads\data.csv"
+df = pd.read_csv(file_path)
+
+
+# In[3]:
+
+
+# Drop unnecessary columns
+df = df.drop(columns=['id', 'Unnamed: 32'])
+
+
+# In[4]:
+
+
+# Encode the 'diagnosis' column (M=1, B=0)
+from sklearn.preprocessing import LabelEncoder
+label_encoder = LabelEncoder()
+df['diagnosis'] = label_encoder.fit_transform(df['diagnosis'])
+
+
+# In[5]:
+
+
+# Handle missing values by dropping rows with null values
+df = df.dropna()
+
+
+# In[6]:
+
+
+# Split the dataset into features (X) and target variable (y)
+X = df.drop(columns=['diagnosis'])
+y = df['diagnosis']
+
+
+# In[7]:
+
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+# In[8]:
+
+
+# Standardize the features using StandardScaler
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
+# In[9]:
+
+
+# Apply PCA
+pca = PCA(n_components=10)  # You can adjust the number of components
+X_train_pca = pca.fit_transform(X_train_scaled)
+X_test_pca = pca.transform(X_test_scaled)
+
+
+# In[10]:
+
+
+# Build and train the Naive Bayes model
+naive_bayes_model = GaussianNB()
+naive_bayes_model.fit(X_train_pca, y_train)
+
+
+# In[11]:
+
+
+# Make predictions on the test set
+y_pred = naive_bayes_model.predict(X_test_pca)
+
+
+# In[12]:
+
+
+# Print accuracy and classification report
+accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred)
+
+print(f"Accuracy: {accuracy:.4f}")
+print("Classification Report:\n", report)
+
+
+# In[ ]:
+
+
+
+
